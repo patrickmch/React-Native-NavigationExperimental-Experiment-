6594992f5f496bc37a160ac91cab69bc19a916fb
plc-patrick <patrick@privatelabelcampaigns.com>
2017-01-12 21:17:34 +0700

    combines getData()and InitialNavState in to one file for better organization

--- app/reducer.js
+++ app/reducer.js
@@ -1,49 +1,6 @@
-import InitialNavState from './state/InitialNavState'
+import { InitialNavState, getData } from './state/NavState'
 import { FIRST_ACTION, SECOND_ACTION } from './actions'
 import { combineReducers } from 'redux'
-
-
-function getData(key){
-  switch(key){
-    //TODO find out why index must only be '1' for all cases
-    //returns error 'there should only be one scene active, not 0' if index is changed
-    case 'First':
-      return {
-        index: 1,
-        key: Math.random(),
-        title: 'First Scene',
-        routes: [
-          { key: 'New', title: 'New' },
-          { key: 'Newer', title: 'Newer' },
-        ],
-        info: 'A bunch of things about something. We cannot be sure what that is yet'
-      }
-    case 'Second':
-      return {
-        index: 1,
-        key: Math.random(),
-        routes: [
-          { key: 'Stoff', title: 'Stoff' },
-          { key: 'Get Out', title: 'Get Out' },
-        ],
-        title: 'Second Scene',
-        info: 'A bunch of things about something. We cannot be sure what that is yet'
-      }
-    case 'Third':
-      return {
-        index: 1,
-        key: Math.random(),
-        routes: [
-          { key: 'Ceck', title: 'Ceck' },
-          { key: 'Junk', title: 'Junk' },
-        ],
-        title: 'Third Scene',
-        info: 'A bunch of things about something. We cannot be sure what that is yet'
-      }
-    default:
-      return 'hello'
-  }//switch
-}//getData
 
 function navigationState(state = InitialNavState, action){
     switch(action.type){
---
+++ app/state/NavState.js
@@ -0,0 +1,55 @@
+export const InitialNavState = {
+  data: {
+      index: 0,
+      routes: [
+        { key: 'First', title: 'First' },
+        { key: 'Second', title: 'Second' },
+        { key: 'Third', title: 'Third' },
+      ]//routes
+  }//data
+}//InitialNavState
+
+
+//getData: function to return routes and associated data
+//called inside the reducer and passed a key based on which item was clicked
+export const getData = (key) => {
+  switch(key){
+    //TODO find out why index must only be '1' for all cases
+    //returns error 'there should only be one scene active, not 0' if index is changed
+    case 'First':
+      return {
+        index: 1,
+        key: Math.random(),
+        title: 'First Scene',
+        routes: [
+          { key: 'New', title: 'New' },
+          { key: 'Newer', title: 'Newer' },
+        ],
+        info: 'A bunch of things about something. We cannot be sure what that is yet'
+      }
+    case 'Second':
+      return {
+        index: 1,
+        key: Math.random(),
+        routes: [
+          { key: 'Stoff', title: 'Stoff' },
+          { key: 'Get Out', title: 'Get Out' },
+        ],
+        title: 'Second Scene',
+        info: 'A bunch of things about something. We cannot be sure what that is yet'
+      }
+    case 'Third':
+      return {
+        index: 1,
+        key: Math.random(),
+        routes: [
+          { key: 'Ceck', title: 'Ceck' },
+          { key: 'Junk', title: 'Junk' },
+        ],
+        title: 'Third Scene',
+        info: 'A bunch of things about something. We cannot be sure what that is yet'
+      }
+    default:
+      return 'hello'
+  }//switch
+}//getData
