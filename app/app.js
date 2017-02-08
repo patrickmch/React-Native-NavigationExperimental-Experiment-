b40c0b74cab706c56f6378fc52b483e4842da424
plc-patrick <patrick@privatelabelcampaigns.com>
2017-02-07 19:00:40 -0800

    adds redux-thunk to the project

--- app/app.js
+++ app/app.js
@@ -8,6 +8,7 @@ import {
 } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import thunk from 'redux-thunk'
 import reducer from './reducer'
 import AppContainer from './containers/AppContainer'
 
